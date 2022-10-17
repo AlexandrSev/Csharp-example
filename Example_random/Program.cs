d88f4efa22de8009dc61f77a2adce4ebe3934899
Alexandr <canek.seva@gmail.com>
2022-10-17 16:04:59 +0300

    add Example004 div and random

---
+++ Example_random/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 50); // 1 2 3 4 ... 49
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 50);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
