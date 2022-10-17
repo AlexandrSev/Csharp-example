f571f207ee867e3846a143b309f545476de125a4
Alexandr <canek.seva@gmail.com>
2022-10-17 16:21:28 +0300

    add Example005 Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "александр")
+{
+    Console.WriteLine("Ура, это же ВЕЛИКОЛЕПНЫЙ АЛЕКСАНДР!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
